547dc6bf44d0209d180962f19289f9d137bf478e
John Mehler <johnmehler@gmail.com>
2018-12-16 21:05:23 -0800

    fixed function.py

--- introduction/function.py
+++ introduction/function.py
@@ -1,11 +1,13 @@
 def is_leap(year):
     leap = False
     
-    if year%4==0:
-      if year%400 is 0:
-        return leap
-      if year%1000 is 0:
-        return False
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+        
     return leap
 
 year = int(input())
